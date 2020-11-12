2e43c55aa8e2388420ed88ced6c68628ba21fe67
Victor Nikliaiev <victor.nikliaiev@gmail.com>
2020-11-12 09:10:04 +0200

    Hoop up some requests

--- app/products/pages/products/[productId].tsx
+++ app/products/pages/products/[productId].tsx
@@ -3,6 +3,7 @@ import Layout from "app/layouts/Layout"
 import { Link, useRouter, useQuery, useParam, BlitzPage, useMutation } from "blitz"
 import getProduct from "app/products/queries/getProduct"
 import deleteProduct from "app/products/mutations/deleteProduct"
+import { requests } from "cypress/types/sinon"
 
 export const Product = () => {
   const router = useRouter()
@@ -13,8 +14,37 @@ export const Product = () => {
   return (
     <div>
       <h1>Product {product.id}</h1>
-      <pre>{JSON.stringify(product, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(product, null, 2)}</pre> */}
+      <header className="flex flex-row mb-4 items-center border">
+        <h2 className="text-base uppercase tracking-wider leading-tight font-semibold text-gray-600">
+          feature requests:
+        </h2>
 
+        <span className="ml-auto">
+          <Link href="/requests/new">
+            <a className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded text-white font-bold">
+              New request
+            </a>
+          </Link>
+        </span>
+      </header>
+      <ul className="space-y-4 p-4 bg-gray-200 rounded">
+        {product.requests.map((request) => {
+          return (
+            <li className="p-4 shadow rounded flex flex-row space-x-4 bg-white">
+              <div className="border rounded">
+                <button className="flex flex-col space-y-4 p-3 rounded shadow-sm hover:bg-yellow-200">
+                  <span>123</span> <span>Vote</span>
+                </button>
+              </div>
+              <div className="flex flex-column">
+                <span className="text-xl">{request.title}</span>
+                <span className="text-xl">{request.description}</span>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
       <Link href="/products/[productId]/edit" as={`/products/${product.id}/edit`}>
         <a>Edit</a>
       </Link>
